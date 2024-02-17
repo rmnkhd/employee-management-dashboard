2a0eec74bfb09a98d38b07ce5f6e5efb483cdd0a
ramin <49345856+rmnkhd@users.noreply.github.com>
2024-02-17 16:11:51 +0330

    fix(page.jsx): link to employees page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,18 @@
-
+import Link from "next/link";
+import "./page.module.css";
 
 export default function Home() {
-  return (
-      <h1>Hi</h1>
-  );
+    return (
+        <div className="d-flex mt-5 align-items-center justify-content-center">
+            <div className='mt-5'>
+                <h4>Welcome To Employee Management </h4>
+                <span>
+                    <Link className=' d-flex align-items-start text-decoration-none' href="/employees">
+                        Employee Management
+                        <i className="ms-2 fs-5 bi-arrow-right"/>
+                     </Link>
+            </span>
+            </div>
+        </div>
+    );
 }
